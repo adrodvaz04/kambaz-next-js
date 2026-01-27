82d94ef07bd40a8e6a0c53a18598510a6e786110
adrodvaz04 <isyla.adrian@gmail.com>
2026-01-27 16:53:58 -0500

    complete lab until flex

---
+++ app/labs/lab2/Float.tsx
@@ -0,0 +1,88 @@
+export default function Float() {
+  return (
+    <div id="wd-float-divs">
+      <h2>Float</h2>
+      <div>
+        <img
+          className="wd-float-right"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic ...
+        Aenean placerat purus risus, eu vehicula mauris euismod et. Nulla
+        commodo, ligula fringilla cursus condimentum, augue magna sagittis
+        tellus, quis semper diam massa vel turpis. Sed consectetur est quis orci
+        interdum molestie. Phasellus sodales eleifend diam a pulvinar.
+        Suspendisse ac eros ipsum. Quisque sagittis tincidunt posuere. Nunc
+        malesuada sagittis lectus, eu dignissim nibh varius ut. Pellentesque
+        sollicitudin urna in sapien consequat convallis. Lorem ipsum, dolor sit
+        amet consectetur adipisicing elit. Eius hic ...
+        <img
+          className="wd-float-left"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic ...
+        Mauris non vulputate orci. Etiam dignissim enim quam, id tempor est
+        lobortis eget. Integer varius volutpat ultrices. Curabitur ac mauris et
+        metus ultricies finibus at eu sem. Quisque commodo mollis scelerisque.
+        Ut in elit leo. Phasellus elementum est nec ullamcorper fringilla.
+        Aliquam in risus egestas, luctus sapien id, mollis orci. Nullam
+        condimentum in arcu vel convallis. Integer congue nunc arcu, vel luctus
+        tellus facilisis id. In tincidunt mauris id quam sodales, a rhoncus
+        felis pharetra. Cras sapien ligula, iaculis sit amet volutpat sed,
+        gravida vel felis. Pellentesque vitae vulputate nibh. Donec ultrices
+        nunc eu nisl auctor, in ullamcorper lectus tempor. Vivamus quis dolor id
+        dui aliquet auctor. Lorem ipsum, dolor sit amet consectetur adipisicing
+        elit. Eius hic ...
+        <img
+          className="wd-float-right"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic ...
+        Donec quis nibh ligula. In ornare urna non dolor sollicitudin volutpat
+        sit amet ac quam. Praesent hendrerit orci dictum nisi rutrum maximus.
+        Sed euismod, nunc in venenatis imperdiet, leo elit ullamcorper tellus,
+        cursus sodales ante diam ac dolor. Proin ac eros posuere, faucibus eros
+        a, molestie dolor. Aliquam nec arcu auctor, facilisis nunc in,
+        sollicitudin diam. Etiam ullamcorper lectus id ex viverra imperdiet. In
+        sit amet massa pharetra, euismod libero sit amet, laoreet turpis. Nunc
+        molestie est id odio dictum interdum. Class aptent taciti sociosqu ad
+        litora torquent per conubia nostra, per inceptos himenaeos. Donec arcu
+        nunc, luctus quis lectus sed, eleifend malesuada ante. Nullam lectus
+        leo, condimentum sit amet turpis non, rutrum vestibulum velit. Quisque
+        mollis nisl bibendum odio hendrerit condimentum. Lorem ipsum, dolor sit
+        amet consectetur adipisicing elit. Eius hic ...
+        <img
+          className="wd-float-left"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius hic ...
+        Proin dignissim metus et lectus pretium suscipit. Nam sed turpis
+        imperdiet, mattis ligula sit amet, elementum orci. Cras luctus risus ac
+        ex elementum malesuada. Fusce at scelerisque sapien, at ullamcorper
+        metus. Vivamus non mauris at nisi fermentum elementum. Morbi at lacinia
+        urna. Phasellus porta sagittis ornare. Suspendisse ut massa et dui
+        malesuada mollis. Vestibulum eget elit non orci fermentum consequat sed
+        vel lectus. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        Eius hic ...
+        <div className="wd-float-done"></div>
+      </div>
+      <br />
+      <div>
+        <div className="wd-float-left wd-dimension-portrait wd-bg-color-yellow">
+          Yellow{" "}
+        </div>
+        <div className="wd-float-left wd-dimension-portrait wd-bg-color-blue wd-fg-color-white">
+          Blue{" "}
+        </div>
+        <div className="wd-float-left wd-dimension-portrait wd-bg-color-red">
+          Red{" "}
+        </div>
+        <img
+          className="wd-float-right"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        />
+        <div className="wd-float-done"></div>
+      </div>
+    </div>
+  );
+}
