a0206271dcc8839915b45ea31589de3e3440880e
adrodvaz04 <isyla.adrian@gmail.com>
2026-04-19 18:37:19 -0400

    refactor(ui, api): join quiz-take logic with editor, list logic

--- app/(kambaz)/store.ts
+++ app/(kambaz)/store.ts
@@ -5,7 +5,7 @@ import accountReducer from "./account/reducer";
 import assignmentsReducer from "./courses/[cid]/assignments/reducer";
 import enrollmentsReducer from "./enrollments/reducer";
 import quizzesReducer from "./courses/[cid]/quizzes/reducer";
-import takeQuizReducer from "./courses/[cid]/quizzes/[qid]/take/reducer";
+import takeQuizReducer from "./courses/[cid]/quizzes/[qid]/take-quiz/reducer";
 
 const store = configureStore({
   reducer: { coursesReducer, modulesReducer, accountReducer, assignmentsReducer, enrollmentsReducer, quizzesReducer, takeQuizReducer},
