56f142711158150fcf0f6b925d717baf69876624
adrodvaz04 <isyla.adrian@gmail.com>
2026-04-19 01:17:56 -0400

    refactor(ui): refactor question editor component

--- app/(kambaz)/courses/[cid]/quizzes/[qid]/details/page.tsx
+++ app/(kambaz)/courses/[cid]/quizzes/[qid]/details/page.tsx
@@ -1,14 +1,10 @@
 "use client";
 
-import { v4 as uuidv4 } from "uuid";
 import { RootState } from "@/app/(kambaz)/store";
 import { redirect, useParams, usePathname } from "next/navigation";
 import {
-  ChangeEvent,
-  ReactElement,
-  ReactHTMLElement,
   useEffect,
-  useState,
+  useState
 } from "react";
 import {
   Button,
@@ -19,18 +15,19 @@ import {
   Table,
   Tabs,
 } from "react-bootstrap";
+import { FaPlus } from "react-icons/fa6";
 import { useDispatch, useSelector } from "react-redux";
+import { v4 as uuidv4 } from "uuid";
 import * as quizzesClient from "../../client";
+import { setQuizzes } from "../../reducer";
 import {
   AssignmentGroup,
-  Quiz,
-  QuizType,
-  QuizQuestion,
   QuestionType,
-  Answer,
+  Quiz,
+  QuizQuestion,
+  QuizType
 } from "../../types";
-import { setQuizzes } from "../../reducer";
-import { FaMinus, FaPencil, FaPlus } from "react-icons/fa6";
+import QuestionEditor from "./questionEditor";
 
 export default function QuizEditor() {
   const { cid, qid } = useParams();
@@ -85,15 +82,8 @@ export default function QuizEditor() {
         },
   );
 
-  const [questions, setQuestions] = useState<QuizQuestion[]>(
-    quiz.questions ?? [],
-  );
-
-  // used for question editing
-  const [editedQuestion, setEditedQuestion] = useState<
-    QuizQuestion | undefined
-  >();
-  const [editedAnswers, setEditedAnswers] = useState<Answer[] | undefined>();
+  // for question editor
+  const [questions, setQuestions] = useState<QuizQuestion[]>(quiz.questions ?? []);
 
   useEffect(() => {
     // if not faculty, redirect
@@ -128,7 +118,10 @@ export default function QuizEditor() {
   const onSaveQuiz = async () => {
     const quizToSave = {
       ...quiz,
-      questions: questions,
+      points: questions?.reduce((acc: number, q: QuizQuestion) => {
+        return acc + q.points;
+      }, 0) ?? 0,
+      questions: questions ?? [],
       dueDate:
         quiz.dueDate instanceof Date
           ? quiz.dueDate.toISOString()
@@ -142,6 +135,8 @@ export default function QuizEditor() {
           ? quiz.availableUntil.toISOString()
           : quiz.availableUntil,
     };
+
+    console.log(quizToSave);
 
     if (newQuiz) {
       const newQuizData = await quizzesClient.createQuiz(quizToSave);
@@ -168,110 +163,21 @@ export default function QuizEditor() {
       questionType: QuestionType.MULTIPLE_CHOICE,
       points: 0,
       question: "Question here.",
-      answers: [{ _id: uuidv4(), text: "Answer here", correct: true }],
+      answers: [],
     };
     setQuestions([...questions, newQuestion]);
-    setEditedQuestion(newQuestion);
   };
 
-  const onNewAnswer = () => {
-    if (editedQuestion?.questionType === QuestionType.TRUE_FALSE) return; // if true/false, do not allow adding answer
-
-    const newAnswer: Answer = {
-      _id: uuidv4(),
-      text: "Answer here.",
-      correct: editedQuestion?.questionType === QuestionType.FILL_IN_BLANK, // i.e, set true for fill-in-blank, false for multiple choice
-    };
-
-    const updatedAnswers = [...(editedAnswers ?? []), newAnswer];
-    setEditedAnswers(updatedAnswers);
-    setEditedQuestion({
-      ...editedQuestion!,
-      answers: updatedAnswers,
-    });
+  const onDeleteQuestion = (questionId: string) => {
+    setQuestions(questions.filter((q: QuizQuestion) => q._id !== questionId));
   };
 
-  const onSelectAnswer = (answerId: string) => {
-    const questionType = editedQuestion?.questionType;
-
-    switch (questionType) {
-      case (QuestionType.TRUE_FALSE, QuestionType.MULTIPLE_CHOICE): // set one true, all others false
-        setEditedAnswers(
-          editedAnswers?.map((a: Answer) =>
-            a._id === answerId ? { ...a, correct: true } : a,
-          ),
-        );
-        break;
-      case QuestionType.FILL_IN_BLANK:
-        setEditedAnswers(
-          editedAnswers?.map((a: Answer) => {
-            return { ...a, correct: true };
-          }),
-        );
-        break;
-    }
-  };
-
-  const onChangeQuestionType = (e: ChangeEvent<HTMLSelectElement>) => {
-    const newType = e.target.value as QuestionType;
-    let updatedAnswers: Answer[];
-
-    switch (newType) {
-      case QuestionType.TRUE_FALSE:
-        updatedAnswers = [
-          { _id: uuidv4(), text: "True", correct: true },
-          { _id: uuidv4(), text: "False", correct: false },
-        ];
-        break;
-      case QuestionType.MULTIPLE_CHOICE:
-        updatedAnswers = [
-          { _id: uuidv4(), text: "Option 1", correct: false },
-          { _id: uuidv4(), text: "Option 2", correct: false },
-        ];
-        break;
-      case QuestionType.FILL_IN_BLANK:
-        updatedAnswers = [{ _id: uuidv4(), text: "Blank", correct: true }];
-        break;
-    }
-
-    const updatedQuestion: QuizQuestion = {
-      ...editedQuestion!,
-      questionType: newType,
-      answers: updatedAnswers,
-    };
-
-    setEditedQuestion(updatedQuestion);
-    setEditedAnswers(updatedAnswers);
-  };
-
-  const onDeleteQuestion = () => {
-    if (editedQuestion) {
-      setQuestions(
-        questions.filter((q: QuizQuestion) => q._id !== editedQuestion!._id),
-      );
-
-      // reset states
-      setEditedQuestion(undefined);
-      setEditedAnswers(undefined);
-    }
-  };
-
-  const onUpdateQuestion = () => {
-    if (editedQuestion) {
-      setEditedQuestion({
-        ...editedQuestion,
-        answers: editedAnswers ?? editedQuestion.answers,
-      });
-      setQuestions(
-        questions.map((q: QuizQuestion) =>
-          q._id === editedQuestion._id ? editedQuestion : q,
-        ),
-      );
-
-      // reset states
-      setEditedQuestion(undefined);
-      setEditedAnswers(undefined);
-    }
+  const onUpdateQuestion = (question: QuizQuestion) => {
+    setQuestions(
+      questions.map((q: QuizQuestion) =>
+        q._id === question._id ? question : q,
+      ),
+    );
   };
 
   return (
@@ -333,12 +239,11 @@ export default function QuizEditor() {
                       defaultValue={quiz.assignment_group}
                     >
                       {Object.values(AssignmentGroup)
-
                         .map((v) => (
                           <option
                             key={v}
                             onClick={() =>
-                              setQuiz({ ...quiz!, assignment_group: v })
+                              setQuiz({ ...quiz, assignment_group: v })
                             }
                           >
                             {" "}
@@ -560,229 +465,14 @@ export default function QuizEditor() {
               </tbody>
             </Table>
           </Tab>
-          {/* 
-                      
-                      
-                      
-                      QUESTIONS SECTION 
-                      
-                      
-                      
-                      
-                      */}
           <Tab eventKey={"questions"} title="Questions">
-            {questions.map((question) => (
-              <div
-                key={question._id}
-                className="mx-5 mb-5 border border-2 fs-5"
-              >
-                <div className="d-flex gap-3 p-3" id="question-editor-header">
-                  <FormControl
-                    size="lg"
-                    defaultValue={question.title}
-                    disabled={editedQuestion?._id !== question._id}
-                    onChange={(e) =>
-                      setEditedQuestion({ ...question, title: e.target.value })
-                    }
-                  />
-                  <Form.Select
-                    size="lg"
-                    defaultValue={question.questionType}
-                    disabled={editedQuestion?._id !== question._id}
-                    onChange={onChangeQuestionType}
-                  >
-                    {Object.values(QuestionType).map((v) => (
-                      <option key={v} value={v}>
-                        {" "}
-                        {v}{" "}
-                      </option>
-                    ))}
-                  </Form.Select>
-                  <span className="mt-2"> Pts </span>
-                  <FormControl
-                    size="lg"
-                    disabled={editedQuestion?._id !== question._id}
-                    defaultValue={question.points}
-                    onChange={(e) =>
-                      setEditedQuestion({
-                        ...question,
-                        points: parseInt(e.target.value),
-                      })
-                    }
-                  ></FormControl>
-                  <Button
-                    size="lg"
-                    variant={
-                      editedQuestion?._id === question._id
-                        ? "secondary"
-                        : "primary"
-                    }
-                    className="d-flex"
-                    disabled={editedQuestion?._id === question._id}
-                    onClick={() => {
-                      setEditedQuestion(question);
-                      setEditedAnswers(question.answers);
-                    }}
-                  >
-                    {" "}
-                    <FaPencil className="me-3 mt-1"></FaPencil> Edit{" "}
-                  </Button>
-                </div>
-                <div
-                  id="quiz-editor-body"
-                  hidden={editedQuestion?._id !== question._id}
-                >
-                  <hr />
-                  <div className="px-4 mb-4">
-                    <h4> Question: </h4>
-                    <FormControl
-                      size="lg"
-                      defaultValue={editedQuestion?.question}
-                      placeholder="Enter text..."
-                      onChange={(e) =>
-                        setEditedQuestion({
-                          ...editedQuestion!,
-                          question: e.target.value,
-                        })
-                      }
-                    ></FormControl>
-                  </div>
-
-                  <div className="px-5 mb-4">
-                    <div className="d-flex gap-3 mb-4">
-                      <h4 className="mt-1"> Answers: </h4>
-                      <Button
-                        className="d-flex"
-                        variant="primary"
-                        size="lg"
-                        hidden={
-                          editedQuestion?.questionType ===
-                          QuestionType.TRUE_FALSE
-                        }
-                        onClick={onNewAnswer}
-                      >
-                        <FaPlus></FaPlus>
-                      </Button>
-                    </div>
-
-                    {editedQuestion?.questionType === QuestionType.TRUE_FALSE ? (
-                      <div>
-                        {editedAnswers?.map((answer: Answer) => (
-                          <Form.Check
-                            key={answer._id}
-                            defaultChecked={answer.correct}
-                            name={editedQuestion.title}
-                            type="radio"
-                            label={
-                              <span>
-                                {answer.text}
-                                {answer.correct && (
-                                  <span className="ms-2 text-success text-nowrap">
-                                    Correct Answer
-                                  </span>
-                                )}
-                              </span>
-                            }
-                            checked={answer.correct}
-                            onChange={() => {
-                              onSelectAnswer(answer._id);
-                            }}
-                          />
-                        ))}
-                      </div>
-                    ) : (
-                      <div>
-                        {editedAnswers?.map((answer: Answer, idx) => (
-                          <div key={idx} className="d-flex gap-4 mb-3">
-                            <Form.Check
-                              label={
-                                answer.correct &&
-                                editedQuestion?.questionType !==
-                                  QuestionType.FILL_IN_BLANK && (
-                                  <span className="pt-2 text-success text-nowrap">
-                                    {" "}
-                                    Correct Answer{" "}
-                                  </span>
-                                )
-                              }
-                              defaultChecked={answer.correct}
-                              className="pt-2"
-                              type="radio"
-                              name={answer.text}
-                              multiple={
-                                editedQuestion?.questionType ===
-                                QuestionType.FILL_IN_BLANK
-                              }
-                              hidden={
-                                editedQuestion?.questionType ===
-                                QuestionType.FILL_IN_BLANK
-                              }
-                              onClick={() => {
-                                onSelectAnswer(answer._id);
-                              }}
-                            ></Form.Check>
-
-                            <FormControl
-                              size="lg"
-                              defaultValue={answer.text}
-                              placeholder="Enter text..."
-                              onChange={(e) => {
-                                const updatedAnswers = editedAnswers.map((a) =>
-                                  a._id === answer._id
-                                    ? { ...answer, text: e.target.value }
-                                    : a,
-                                );
-                                setEditedQuestion({
-                                  ...editedQuestion!,
-                                  answers: updatedAnswers,
-                                });
-                                setEditedAnswers(updatedAnswers);
-                              }}
-                            ></FormControl>
-                            <Button
-                              size="lg"
-                              variant="danger"
-                              hidden={
-                                editedQuestion?.questionType ===
-                                QuestionType.TRUE_FALSE
-                              }
-                              onClick={() => {
-                                const filteredAnswers =
-                                  editedAnswers?.filter(
-                                    (a) => a._id !== answer._id,
-                                  ) ?? [];
-                                setEditedAnswers(filteredAnswers);
-                                setEditedQuestion({
-                                  ...editedQuestion!,
-                                  answers: filteredAnswers,
-                                });
-                              }}
-                            >
-                              {" "}
-                              <FaMinus></FaMinus>{" "}
-                            </Button>
-                          </div>
-                        ))}
-                      </div>
-                    )}
-                    <hr />
-                    <Button size="lg" onClick={onUpdateQuestion}>
-                      {" "}
-                      Update Question{" "}
-                    </Button>
-                    <Button
-                      size="lg"
-                      variant="danger"
-                      className="float-end"
-                      onClick={() => {
-                        onDeleteQuestion();
-                      }}
-                    >
-                      {" "}
-                      Delete Question{" "}
-                    </Button>
-                  </div>
-                </div>
+            {questions?.map((question, idx) => (
+              <div key={idx}>
+                <QuestionEditor
+                  quizQuestion={question}
+                  onDeleteQuestion={onDeleteQuestion}
+                  onUpdateQuestion={onUpdateQuestion}
+                ></QuestionEditor>
               </div>
             ))}
             <Button size="lg" variant="secondary" onClick={onNewQuestion}>
